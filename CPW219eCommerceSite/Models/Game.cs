6786a0e9426be788c9166fa1040fa91c061cab12
VladProgrammer08 <bumbuv136@gmail.com>
2024-01-27 19:31:41 -0800

    Added game class

---
+++ CPW219eCommerceSite/Models/Game.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
+
+namespace CPW219eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents a single game for available for purchase
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// The unique identifier for each game product
+        /// (Primary Key)
+        /// </summary>
+        [Key]
+        public int GameId { get; set; }
+
+        /// <summary>
+        /// The official title of the Video Game
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The sales price of the game
+        /// </summary>
+        [Range(0, 1000)]
+        public double Price { get; set; }
+
+        // Todo: Add rating
+    }
+}

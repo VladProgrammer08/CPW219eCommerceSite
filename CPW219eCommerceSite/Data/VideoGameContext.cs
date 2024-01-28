2643e184b5776a8d166249097c6f546f8896928e
VladProgrammer08 <bumbuv136@gmail.com>
2024-01-27 20:24:43 -0800

    Added Db Context class

---
+++ CPW219eCommerceSite/Data/VideoGameContext.cs
@@ -0,0 +1,15 @@
+﻿using CPW219eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPW219eCommerceSite.Data
+{
+    public class VideoGameContext: DbContext
+    {
+        public VideoGameContext(DbContextOptions<VideoGameContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}

9cecba49aebd4f4c9e92fcd0658d0190cf9b36b7
VladProgrammer08 <bumbuv136@gmail.com>
2024-02-03 20:55:18 -0800

    Create game fully functional, added some styling

--- CPW219eCommerceSite/Controllers/GamesController.cs
+++ CPW219eCommerceSite/Controllers/GamesController.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW219eCommerceSite.Data;
+using CPW219eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW219eCommerceSite.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly VideoGameContext _context;
+        public GamesController(VideoGameContext context)
+        {
+            _context = context;
+        }
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Game g)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Games.Add(g); // Prepares insert
+                _context.SaveChanges(); // Executes pending insert
+
+                ViewData["Message"] = $"{g.Title} was added successfully!";
+                return View();
+            }
+            return View(g);
         }
     }
 }

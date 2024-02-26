0090aef49462ad726342c3837ec1f53e1e482a3f
VladProgrammer08 <bumbuv136@gmail.com>
2024-02-25 18:50:58 -0800

    Add Member class

---
+++ CPW219eCommerceSite/Models/Member.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219eCommerceSite.Models
+{
+    public class Member
+    {
+        [Key]
+        public int MemberID { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Username { get; set; }
+    }
+
+
+}

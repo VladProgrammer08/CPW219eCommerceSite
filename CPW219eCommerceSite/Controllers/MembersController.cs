a843b475dd3b1d5762b6602f37b851806ddb46df
VladProgrammer08 <bumbuv136@gmail.com>
2024-02-25 21:27:22 -0800

    Added HttPost for Register

--- CPW219eCommerceSite/Controllers/MembersController.cs
+++ CPW219eCommerceSite/Controllers/MembersController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW219eCommerceSite.Data;
+using CPW219eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW219eCommerceSite.Controllers
 {
     public class MembersController : Controller
     {
+        private readonly VideoGameContext _context;
+
+        public MembersController(VideoGameContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel regModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Map RegisterViewModel data to Member object
+                Member newMember = new()
+                {
+                    Email = regModel.Email,
+                    Password = regModel.Password,
+                    
+                };
+
+                _context.Members.Add(newMember);
+                await _context.SaveChangesAsync();
+
+                // Redirect to home page
+                return RedirectToAction("Index", "Home");
+            }
+            return View(regModel);
         }
     }
 }

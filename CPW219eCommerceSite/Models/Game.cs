31acf149e048cb02186b7ccfca8bc59568aee4f8
VladProgrammer08 <bumbuv136@gmail.com>
2024-03-14 22:35:50 -0700

    Added a ViewModel for the shopping cart summary page

--- CPW219eCommerceSite/Models/Game.cs
+++ CPW219eCommerceSite/Models/Game.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection.Metadata;
 
 namespace CPW219eCommerceSite.Models
@@ -28,5 +29,20 @@ namespace CPW219eCommerceSite.Models
         public double Price { get; set; }
 
         // Todo: Add rating
+    }
+
+    /// <summary>
+    /// A single video game that has been added to the 
+    /// users shopping cart cookie
+    /// </summary>
+
+    public class CartGameViewModel
+    {
+        [Key]
+        public int GameId { get; set; }
+
+        public string Title { get; set; }
+
+        public double Price { get; set; }
     }
 }
